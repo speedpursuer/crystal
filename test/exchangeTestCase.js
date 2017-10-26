07720f5abf4aab27995ec207a864de8ffb4aa77a
speedpursuer <23leixing@163.com>
2017-10-26 16:43:21 +0800

    测试bitstamp

--- test/exchangeTestCase.js
+++ test/exchangeTestCase.js
@@ -40,7 +40,7 @@ describe.only('测试 exchange', async function() {
   	describe.only('获取市场深度', async function() {  		
     	it('返回数量压缩不超过5', async function() {   
     		// ['Bitfinex', 'Poloniex', 'Bittrex', 'Bitstamp', 'okcoinusd']   		      		
-    		var exchange = new Exchange('Bitfinex', "BTC", true)
+    		var exchange = new Exchange('bitstamp', "BTC", true)
     		var orderBook = await exchange.fetchOrderBook()
     		util.log(orderBook)		
     		orderBook.bids.length.should.not.be.above(5)
