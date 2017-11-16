b80c1edbf453a7d745e26c26696a3371b82f15ac
speedpursuer <23leixing@163.com>
2017-11-16 10:21:47 +0800

    update

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ async function main(){
     global.realMode = true
     global.realSim = true
 	try {          
-        var exchangeIDs = ['hitbtc', 'bittrex', 'okex', 'poloniex', 'huobipro']    
+        var exchangeIDs = ['bittrex', 'okex', 'poloniex', 'huobipro']    
         // var exchangeIDs = ['hitbtc', 'bitfinex', 'bittrex', 'poloniex', 'okex']    
         // var exchangeIDs = ['hitbtc', 'okex', 'poloniex', 'bittrex', 'huobipro']    
         // var trade = new Trade(exchangeIDs, new Hedge("BCH", "BTC"))
@@ -25,15 +25,15 @@ async function test(){
     try {              
         // var backtest = new Backtest("2017-11-01 00:00:00", "2017-11-08 00:00:00", false)
         // var backtest = new Backtest("2017-11-02 09:14:55", "2017-11-03 09:14:55", true)        
-        var backtest = new Backtest("2017-11-09 00:00:00", null, false)
+        var backtest = new Backtest("2017-11-01 00:00:00", null, false)
         // var backtest = new Backtest("2017-11-01 09:14:55", "2017-11-02 09:14:55")
         // await backtest.LTC()
         // await backtest.XMR()
         // await backtest.XRP()
 
         // await backtest.BTC()
-        // await backtest.LTC()
-        await backtest.ETH()
+        await backtest.LTC()
+        // await backtest.ETH()
         // await backtest.BCH()
         // await backtest.BCHTest(['Bitfinex', 'okex'])  
         // await backtest.BCHTest(['Poloniex', 'Bittrex', 'hitbtc', 'okex', 'Bitfinex'])     
