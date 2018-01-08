f818f652d5292f5ff8bd289255b5e2d32695ddf3
speedpursuer <23leixing@163.com>
2018-01-08 14:00:46 +0800

    更新trade config

--- config/tradeConfig.js
+++ config/tradeConfig.js
@@ -29,8 +29,8 @@ const tradeConfig = {
         strategy: HedgeTest,
         strategyConfig: {
             maxAmountOnce: 1,
-            orderRate: 0.06,
-            minMargin: 0.00005,
+            orderRate: 0.1,
+            minMargin: 0.0001,
             maxLoss: -0.001,
             debug: true
         }
@@ -44,8 +44,8 @@ const tradeConfig = {
         strategy: HedgeTest,
         strategyConfig: {
             maxAmountOnce: 1,
-            orderRate: 0.06,
-            minMargin: 0.00002,
+            orderRate: 0.1,
+            minMargin: 0.00004,
             maxLoss: -0.001,
             debug: true
         }
