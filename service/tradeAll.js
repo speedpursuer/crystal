76170448188872745b7664598092d5a7cd2c33ff
speedpursuer <23leixing@163.com>
2018-01-19 17:52:10 +0800

    整合交易wip 2

--- service/tradeAll.js
+++ service/tradeAll.js
@@ -1,24 +1,40 @@
 const _ = require('lodash')
 const util = require ('../util/util.js')
-const TradeBuilder = require('./tradeBuilder')
+const TradeAllBuilder = require('./tradeAllBuilder')
 
 const Interval = 2000
 
 class TradeAll{
-    constructor(tradeName, debug=true){
+    constructor(debug=true){
         this.debug = debug
-        this.tradeBuilder = new TradeBuilder(tradeName, false)
-        this.strategy = this.tradeBuilder.strategy
+        this.tradeBuilder = new TradeAllBuilder(false)
+        // this.strategy = this.tradeBuilder.strategy
     }
 
     async init(){
         this.createExchanges()
-        var list = await util.promiseFor(this.exchanges, 'fetchAccount')
+        await this.getAllInitBalances()
+        // await this.strategy.init(this.exchangeSet)
+        // this.strategy.before()
+    }
 
+    createExchanges() {
+        // this.exchangesIDs = _.sortBy(_.map(this.tradeBuilder.exchanges, function(i) {return i.toLowerCase()}) )
+        this.exchangeSet = this.tradeBuilder.buildAllExchanges()
+        this.exchangesById = this.tradeBuilder.buildExchangesById(this.exchangeSet)
+    }
+
+    async getAllInitBalances() {
+        for(let symbol in this.exchangeSet) {
+            await this.getInitBalances(symbol, this.exchangeSet[symbol])
+        }
+    }
+
+    async getInitBalances(symbol, exchanges) {
+        var list = await util.promiseFor(exchanges, 'fetchAccount')
         if(_.filter(list, function(o) { return (o.balance == 0 && o.stocks == 0) }).length > 0 ) {
             throw new Error("初始账户信息有误")
         }
-
         var total = _.reduce(list, function(result, value, key) {
             result.balance += value.balance
             result.stocks += value.stocks
@@ -26,21 +42,13 @@ class TradeAll{
         }, {balance: 0, stocks: 0})
         this.log(`*********************************************************`)
         this.log(`*********************************************************`)
-        this.log(`******* 获取 ${list.length} 个交易所账户信息, 总钱: ${total.balance}, 总币: ${total.stocks} *******`)
+        this.log(`******* 交易对 ${symbol} 获取 ${list.length} 个交易所账户信息, 总钱: ${total.balance}, 总币: ${total.stocks} *******`)
         this.log(`*********************************************************`)
         this.log(`*********************************************************`)
-
-        await this.strategy.init(this.exchanges)
-        this.strategy.before()
-    }
-
-    createExchanges() {
-        this.exchangesIDs = _.sortBy(_.map(this.tradeBuilder.exchanges, function(i) {return i.toLowerCase()}) )
-        this.exchanges = this.tradeBuilder.buildExchanges(this.exchangesIDs)
     }
 
     async updateOrderBook(){
-        await util.promiseFor(this.exchanges, 'fetchOrderBook')
+        // await util.promiseFor(this.exchanges, 'fetchOrderBook')
     }
 
     async loop(){
@@ -63,7 +71,7 @@ class TradeAll{
             util.log.red("交易初始化失败，程序退出")
             throw e
         }
-        await this.loop()
+        // await this.loop()
     }
 
     async handleError(err) {
